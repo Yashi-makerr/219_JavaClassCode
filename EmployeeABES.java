4a0599061bb47d2d92e1d7fd8aaefc1f135f39cb
Yashi-makerr <ykesarwani41@gmail.com>
2025-08-24 00:17:36 +0530

    Adding more code

---
+++ EmployeeABES.java
@@ -0,0 +1,15 @@
+package ABES;
+import kiet.*;
+public class EmployeeABES extends EmployeeKiet {
+    public static void maim(String[] args){
+        //EmployeeKiet ek=new EmployeeKiet();
+        // ek.Emp_Name="Subhii";
+        // ek.Emp_id=102;
+        // ek.salary=20000;
+        // ek.Organisation="Kiet groups";
+        EmployeeABES ae=new EmployeeABES();
+        ae.Emp_Name="Parull";
+        //ae.salary=10000;
+        ae.Emp_id=1002; 
+    }
+}
